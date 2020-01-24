4cdb1ad293b434e014149f5b9789658019af97df
Mykhailo Petrenko <mihalytch@gmail.com>
2020-01-25 01:36:18 +0200

    Level 2.2. Hey, I Already Did That! Find the loop period in generated sequence.

---
+++ src/main/java/kata/randomid/Solution.java
@@ -0,0 +1,137 @@
+package kata.randomid;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Hey, I Already Did That!
+ * ========================
+ *
+ * Commander Lambda uses an automated algorithm to assign minions randomly to tasks,
+ * in order to keep her minions on their toes.
+ * But you've noticed a flaw in the algorithm - it eventually loops back on itself,
+ * so that instead of assigning new minions as it iterates,
+ * it gets stuck in a cycle of values so that the same minions end up doing the same tasks over and over again.
+ * You think proving this to Commander Lambda will help you make a case for your next promotion.
+ *
+ * You have worked out that the algorithm has the following process:
+ *
+ * 1) Start with a random minion ID n, which is a nonnegative integer of length k in base b
+ * 2) Define x and y as integers of length k.  x has the digits of n in descending order, and y has the digits of n in ascending order
+ * 3) Define z = x - y.  Add leading zeros to z to maintain length k if necessary
+ * 4) Assign n = z to get the next minion ID, and go back to step 2
+ *
+ * For example, given minion ID n = 1211, k = 4, b = 10, then x = 2111, y = 1112 and z = 2111 - 1112 = 0999.
+ * Then the next minion ID will be n = 0999 and the algorithm iterates again: x = 9990, y = 0999 and z = 9990 - 0999 = 8991, and so on.
+ *
+ * Depending on the values of n, k (derived from n), and b, at some point the algorithm reaches a cycle,
+ * such as by reaching a constant value.
+ * For example, starting with n = 210022, k = 6, b = 3, t
+ * he algorithm will reach the cycle of values [210111, 122221, 102212]
+ * and it will stay in this cycle no matter how many times it continues iterating.
+ * Starting with n = 1211,
+ * the routine will reach the integer 6174,
+ * and since 7641 - 1467 is 6174, it will stay as that value no matter how many times it iterates.
+ *
+ * Given a minion ID as a string n representing a nonnegative integer of length k in base b, where 2 <= k <= 9 and 2 <= b <= 10,
+ * write a function solution(n, b) which returns the length of the ending cycle of the algorithm above starting with n.
+ * For instance, in the example above, solution(210022, 3) would return 3,
+ * since iterating on 102212 would return to 210111 when done in base 3.
+ * If the algorithm reaches a constant, such as 0, then the length is 1.
+ *
+ * Test cases
+ * ==========
+ * Your code should pass the following test cases.
+ * Note that it may also be run against hidden test cases not shown here.
+ *
+ * -- Java cases --
+ * Input:
+ * Solution.solution('210022', 3)
+ * Output:
+ *     3
+ *
+ * Input:
+ * Solution.solution('1211', 10)
+ * Output:
+ *     1
+ */
+public class Solution {
+
+    public static char[] reverse(char[] in) {
+        char[] out = new char[in.length];
+        int begin=0;
+        int end = in.length-1;
+
+        while (end >= begin) {
+            out[end] = in[begin];
+            out[begin] = in[end];
+            end--;
+            begin++;
+        }
+
+        return out;
+    }
+
+    public static String generate(String n, int b) {
+        int k = n.length();
+
+        char[] asc = n.toCharArray();
+        Arrays.sort(asc);
+        char[] desc = reverse(asc);
+
+        String x = new String(desc);
+        String y = new String(asc);
+
+        String z = Integer.toString(Integer.parseInt(x, b) - Integer.parseInt(y, b), b);
+
+        if (z.length() == k) {
+            return z;
+        }
+
+        char[] zeroes = new char[k - z.length()];
+        Arrays.fill(zeroes, '0');
+
+        return new String(zeroes) + z;
+    }
+
+    public static int solution(String n, int b) {
+        int index = 0;
+        int diff;
+        int prevDiff = -1;
+        int diffCounter = -1;
+
+        Map<String, Integer> d = new HashMap<String, Integer>();
+        d.put(n, index);
+
+        while (true) {
+            n = generate(n, b);
+            index++;
+
+            if (d.containsKey(n)) {
+                diff = (index - d.get(n));
+
+                if (prevDiff == -1) {
+                    prevDiff = diff;
+                    diffCounter = diff;
+
+                    d.put(n, index);
+
+                    continue;
+                } else if (diff == prevDiff) {
+                    diffCounter--;
+
+                    if (diffCounter == 0) {
+                        return diff;
+                    }
+                } else {
+                    prevDiff = -1;
+                    diffCounter = -1;
+                }
+            }
+
+            d.put(n, index);
+        }
+    }
+}
---
+++ src/test/java/kata/randomid/SolutionTests.java
@@ -0,0 +1,16 @@
+package kata.randomid;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTests {
+    @Test
+    public void baseTest() {
+        Assert.assertEquals(3, Solution.solution("210022", 3));
+    }
+
+    @Test
+    public void baseOneTest() {
+        Assert.assertEquals(1, Solution.solution("1211", 10));
+    }
+}
