3359d40d3117e8034170034ae75fc2ccc0aad677
Mykhailo Petrenko <mihalytch@gmail.com>
2020-01-26 00:41:36 +0200

    Level 3.1. Queue To Do. XOR

---
+++ src/test/java/kata/queuetodo/SolutionTest.java
@@ -0,0 +1,23 @@
+package kata.queuetodo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void positionThree() {
+        Assert.assertEquals(
+            2,
+            Solution.solution(0, 3)
+        );
+    }
+
+    @Test
+    public void positionSeventeen() {
+        Assert.assertEquals(
+            14,
+            Solution.solution(17, 4)
+        );
+    }
+}
