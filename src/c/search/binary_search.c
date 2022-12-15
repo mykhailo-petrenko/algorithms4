2d908199e88344acd823006e017f920c3e575c18
Mykhailo Petrenko <mikael.petrenko@gmail.com>
2022-12-15 11:20:12 +0100

    C. Update bsearch. Add some tests.

--- src/c/search/binary_search.c
+++ src/c/search/binary_search.c
@@ -18,23 +18,48 @@ int binary_search(int needle, int* nums, int from, int to) {
         }
 
         if (needle > nums[mid]) {
-            lo = mid;
+            lo = mid + 1;
         } else {
             hi = mid;
         }
     }
 
-    return mid;
+    if (needle == nums[mid]) {
+        return lo;
+    }
+
+    return -1;
 }
 
 
 int main() {
-    int* nums = {1,3,5,6,8,9,10,11,13,14,15,18,23,28,50,68,101,505};
+    int nums[] = {1,3,5,6,8,9,10,11,13,14,15,18,23,28,50,68,101,505};
     int N = 18;
     int out;
 
-    out = binary_search(3, nums, 0, N);
-    printf("nums[%d] = %d", out, 3);
+    out = binary_search(1, &nums[0], 0, N);
+    printf("nums[%d] = %d\n", out, 1);
+
+    out = binary_search(3, &nums[0], 0, N);
+    printf("nums[%d] = %d\n", out, 3);
+
+    out = binary_search(68, &nums[0], 0, N);
+    printf("nums[%d] = %d\n", out, 68);
+
+    out = binary_search(101, &nums[0], 0, N);
+    printf("nums[%d] = %d\n", out, 101);
+
+    out = binary_search(505, &nums[0], 0, N);
+    printf("nums[%d] = %d\n", out, 505);
+
+    out = binary_search(-1, &nums[0], 0, N);
+    printf("nums[%d] = %d\n", out, -1);
+
+    out = binary_search(100, &nums[0], 0, N);
+    printf("nums[%d] = %d\n", out, 100);
+
+    out = binary_search(700, &nums[0], 0, N);
+    printf("nums[%d] = %d\n", out, 700);
 
     return 0;
 }
