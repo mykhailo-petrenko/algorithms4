dbcf2ee25940d82ea5547fa5aefb46eb3c8f37e4
Mykhailo Petrenko <mikael.petrenko@gmail.com>
2022-12-30 18:25:34 +0100

    LeetCode. 959. Regions Cut By Slashes (https://leetcode.com/problems/regions-cut-by-slashes/)

--- src/c/uf/959_Regions_Cut_By_Slashes.c
+++ src/c/uf/959_Regions_Cut_By_Slashes.c
@@ -26,11 +26,110 @@ Note that backslash characters are escaped, so a '\' is represented as '\\'.
     1 <= n <= 30
     grid[i][j] is either '/', '\', or ' '.
  * */
+#include <stdlib.h>
+#include "../lib/utest.h"
 
-int regionsBySlashes(char ** grid, int gridSize){
 
+void groupId(int *graph, int *a) {
+    while(graph[*a] != *a) {
+        *a = graph[*a];
+    }
+}
+
+void merge(int *graph, int a, int b) {
+    groupId(graph, &a);
+    groupId(graph, &b);
+
+    if (a > b) {
+        graph[a] = graph[b];
+    } else {
+        graph[b] = graph[a];
+    }
+}
+
+/**
+ *
+ * Cell:
+ *   0
+ *   _
+ * 3|_|1
+ *   2
+ *
+ * @param grid
+ * @param gridSize
+ * @return
+ */
+int regionsBySlashes(char ** grid, int gridSize) {
+    const int GROUPS_COUNT =  gridSize * gridSize * 4;
+    int * graph = malloc(GROUPS_COUNT * sizeof(int));
+    char s;
+    int groupId;
+    int siblingGroupId;
+
+    // Init UF tree
+    for (int i=0; i<GROUPS_COUNT; i++) {
+        graph[i] = i;
+    }
+
+    // Visit each cell
+    for (int y = 0; y < gridSize; y++) {
+        for (int x = 0; x < gridSize; x++) {
+            s = grid[y][x];
+            groupId = ((y * gridSize) + x) * 4;
+
+            // Divide each cell on virtual regions and merge then
+            if (s == ' ') {
+                merge(graph, groupId + 1, groupId + 0);
+                merge(graph, groupId + 2, groupId + 0);
+                merge(graph, groupId + 3, groupId + 0);
+            } else if (s == '\\') {
+                merge(graph, groupId + 1, groupId + 0);
+                merge(graph, groupId + 3, groupId + 2);
+            } else if (s == '/') {
+                merge(graph, groupId + 3, groupId + 0);
+                merge(graph, groupId + 2, groupId + 1);
+            }
+
+            // merge with top cell
+            if (y > 0) {
+                siblingGroupId = (((y - 1) * gridSize) + x) * 4;
+
+                merge(graph, siblingGroupId + 2, groupId + 0);
+            }
+
+            // merge with left cell
+            if (x > 0) {
+                siblingGroupId = ((y * gridSize) + (x - 1)) * 4;
+
+                merge(graph, siblingGroupId + 1, groupId + 3);
+            }
+        }
+    }
+
+    int groupsTotal = 0;
+
+    for (int i=0; i<GROUPS_COUNT; i++) {
+        if (graph[i] == i) groupsTotal++;
+    }
+
+    return groupsTotal;
+}
+
+void doTest(char ** grid, int gridSize, int expected) {
+    int actual = regionsBySlashes(grid, gridSize);
+
+    assert_equals_int("regionsBySlashes(char ** grid, int gridSize)", &expected, &actual);
 }
 
 int main() {
+    char *grid1[] = {" /", "/ "};
+    doTest(grid1, 2, 2);
+
+    char *grid2[] = {" /","  "};
+    doTest(grid2, 2, 1);
+
+    char *grid3[] = {"/\\","\\/"};
+    doTest(grid3, 2, 5);
+
     return 0;
 }
