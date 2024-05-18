596f5e72d01a26b198bdfc3612d18529f177d57a
Mykhailo Petrenko <mikael.petrenko@gmail.com>
2024-05-19 01:00:51 +0200

    LeetCode. 864. Shortest Path to Get All Keys (https://leetcode.com/problems/shortest-path-to-get-all-keys/description/)
    using MinPriorityQueue by the path length. A bit slow.

--- src/js/graph/864_Shortest_Path_to_Get_All_Keys.js
+++ src/js/graph/864_Shortest_Path_to_Get_All_Keys.js
@@ -1,7 +1,7 @@
 /**
 # LeetCode. 864. Shortest Path to Get All Keys (https://leetcode.com/problems/shortest-path-to-get-all-keys/description/)
 
-You are given an m x n grid grid where:
+You are given an m x n grid where:
 
 '.' is an empty cell.
 '#' is a wall.
@@ -19,7 +19,11 @@ and also that the letters used to represent the keys and locks were chosen in th
 Return the lowest number of moves to acquire all keys. If it is impossible, return -1.
 
 ## Example 1:
-  Input: grid = ["@.a..","###.#","b.A.B"]
+  Input: grid = [
+    "@ . a . . ",
+    "# # # . # ",
+    "b . A . B "
+]
   Output: 8
   Explanation: Note that the goal is to obtain all the keys not to open all the locks.
 
@@ -44,12 +48,103 @@ Return the lowest number of moves to acquire all keys. If it is impossible, retu
 
  */
 
+import { MinPriorityQueue } from '@datastructures-js/priority-queue';
+
 /**
  * @param {string[]} grid
  * @return {number}
  */
 var shortestPathAllKeys = function(grid) {
+  const ROWS = grid.length;
+  const COLS = grid[0].length;
+  const N = ROWS * COLS;
 
+  const KEYS = ['a','b','c','d','e','f'];
+  const LOCKS = ['A','B','C','D','E','F'];
+  const isValid = (r, c) => {
+    if (r <0 || c <0 || r >= ROWS || c >= COLS) {
+      return false;
+    }
+
+    return g[r][c] !== '#';
+  };
+  const isKey = (c) => {
+    return KEYS.indexOf(c)+1;
+  };
+
+  const isLock = (c) => {
+    return LOCKS.indexOf(c)+1;
+  };
+
+  const start = [];
+  const _keys = [];
+
+  const g = [];
+  let i =0;
+  for (const row of grid) {
+    g[i] = row.split('');
+
+    for (let k=0; k < COLS; k++) {
+      if (g[i][k] === '@') {
+        start[0] = i;
+        start[1] = k;
+      } else if (isKey(g[i][k])) {
+        _keys.push([i, k]);
+      }
+    }
+
+    i++;
+  }
+
+  const q = new MinPriorityQueue();
+  q.enqueue([...start, 0, new Set(), new Set()], 0);
+
+  while (q.size() > 0) {
+    let [r, c, path, wallet, visitedPrev] = q.dequeue().element;
+    const k = isKey(g[r][c]);
+    const l = isLock(g[r][c]);
+
+    if (visitedPrev.has(`${r}-${c}`)) {
+      continue;
+    }
+    const visited = new Set(visitedPrev);
+    visited.add(`${r}-${c}`);
+
+    if (k !== 0) {
+      if (!wallet.has(k)) {
+        // pick up the key
+        wallet = new Set(wallet);
+        wallet.add(k);
+
+        if (wallet.size === _keys.length) {
+          return path;
+        }
+
+        visited.clear();
+      } else {
+        continue;
+      }
+    } else if (l !== 0) {
+      if (!wallet.has(l)) {
+        continue;
+      }
+    }
+
+    if (isValid(r+1, c)) {
+      q.enqueue([r+1, c, path+1, wallet, visited], path); //visited.size * N + path);
+    }
+    if (isValid(r, c+1)) {
+      q.enqueue([r, c+1, path+1, wallet, visited], path); //visited.size * N + path);
+    }
+    if (isValid(r, c-1)) {
+      q.enqueue([r, c-1, path+1, wallet, visited], path); //visited.size * N + path);
+    }
+    if (isValid(r-1, c)) {
+      q.enqueue([r-1, c, path+1, wallet, visited], path); //visited.size * N + path);
+    }
+  }
+
+  return -1;
 };
 
 export default shortestPathAllKeys;
--- src/js/graph/864_Shortest_Path_to_Get_All_Keys.test.js
+++ src/js/graph/864_Shortest_Path_to_Get_All_Keys.test.js
@@ -1,3 +1,5 @@
+import shortestPathAllKeys from './864_Shortest_Path_to_Get_All_Keys.js';
+
 describe('864. Shortest Path to Get All Keys', () => {
 
   it('should pass Example 1', () => {
@@ -13,6 +15,20 @@ describe('864. Shortest Path to Get All Keys', () => {
   it('should pass Example 3', () => {
     const actual = shortestPathAllKeys(["@Aa"]);
     expect(actual).toEqual(-1);
+  });
+
+  it('should be fast enough', () => {
+    const actual = shortestPathAllKeys(["..#....##.","....d.#.D#","#...#.c...","..##.#..a.","...#....##","#....b....",".#..#.....","..........",".#..##..A.",".B..C.#..@"]);
+    expect(actual).toEqual(19);
+  });
+
+  it('should pass Case 19', () => {
+    const actual = shortestPathAllKeys(["@fedcbBCDEFaA"]);
+    expect(actual).toEqual(11);
+  });
+  it('should pass Case 20', () => {
+    const actual = shortestPathAllKeys([".##..##...","...#.#.#B#",".#.#......",".#....#...","...###...C","#.##..#.#.","...A.c....","#..a.@..##","##..#.....","..#....b.."]);
+    expect(actual).toEqual(10);
   });
 
 });
